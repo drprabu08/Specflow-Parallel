84a82cbfced75e5206269adc9538ec370a80bbee
drprabu08 <65601096+drprabu08@users.noreply.github.com>
2020-05-26 13:22:52 +0530

    Add files via upload

--- NUnitParalllelExecution/NUnitParalllelExecution/ExtentReport/ExtentTestManager.cs
+++ NUnitParalllelExecution/NUnitParalllelExecution/ExtentReport/ExtentTestManager.cs
@@ -1,9 +1,12 @@
 ﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
 using NUnitParalllelExecution.Extent;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
+using TechTalk.SpecFlow;
 
 namespace NUnitParalllelExecution.ExtentReport
 {
@@ -16,25 +19,54 @@ namespace NUnitParalllelExecution.ExtentReport
         private static ExtentTest _childTest;
 
         [MethodImpl(MethodImplOptions.Synchronized)]
-        public static ExtentTest CreateParentTest(string testName, string description = null)
+        public static ExtentTest CreateParentTest(string featureName)
         {
-            Console.WriteLine("Entered Create Parent Test Extent Reports");
-            _parentTest = ExtentManager.Instance.CreateTest(testName, description);
+
+            _parentTest = ExtentManager.Instance.CreateTest<Feature>(featureName);
             return _parentTest;
         }
 
         [MethodImpl(MethodImplOptions.Synchronized)]
-        public static ExtentTest CreateTest(string testName, string description = null)
+        public static ExtentTest CreateTest(string scenarioName)
         {
-            Console.WriteLine("Entered Create Test Extent Report");
-            _childTest = _parentTest.CreateNode(testName, description);
+
+            _childTest = _parentTest.CreateNode<Scenario>(scenarioName);
             return _childTest;
         }
 
         [MethodImpl(MethodImplOptions.Synchronized)]
+        public static void CreateTestStep(string stepType, string stepName)
+        {
+            if(stepType == "Given")
+                _childTest.CreateNode<Given>(stepName);
+            else if (stepType == "Then")
+                _childTest.CreateNode<Then>(stepName);
+            else if (stepType == "When")
+                _childTest.CreateNode<When>(stepName);
+            
+        }
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static void CreateFailedTestStep(string stepType, string stepName, string ex, MediaEntityModelProvider mediaEntity)
+        {
+            if (stepType == "Given")
+                _childTest.CreateNode<Given>(stepName).Fail(ex, mediaEntity);
+            else if (stepType == "Then")
+                _childTest.CreateNode<Then>(stepName).Fail(ex, mediaEntity);
+            else if (stepType == "When")
+                _childTest.CreateNode<When>(stepName).Fail(ex, mediaEntity);
+
+        }
+        [MethodImpl(MethodImplOptions.Synchronized)]
         public static ExtentTest GetTest()
         {
             return _childTest;
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static MediaEntityModelProvider CapturScreenshotANdReturnModel(string name, IWebDriver _driver)
+        {
+            var screenshot = ((ITakesScreenshot)_driver).GetScreenshot().AsBase64EncodedString;
+            return MediaEntityBuilder.CreateScreenCaptureFromBase64String(screenshot, name).Build();
         }
     }
 }
